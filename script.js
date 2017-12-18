d8a1ef19dddf901c77b5e23573068c5c08a0ac2f
mrcreel <michaelrcreel@gmail.com>
2017-12-18 16:47:16 -0600

    Clean up formatting

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 var ele = "";
 var elezIndex = 0;
-/*  var myVar =  */ setInterval(myTimer, 1000);
+/*  var myVar =  setInterval(myTimer, 1000); */
 
 function myTimer() {
   // var minTensZindex = parseInt(document.getElementById("minTens0").style.zIndex);
-  
+
   var secondsOnes;
   var secondsTens = [];
   var minutesOnes = [];
   // var minutesTens = [];
-  
+
   secondsOnes = decrementPaper("seconds-ones");
 
+  console.log(parseInt(document.getElementById("minTens0").style.zIndex));
+
   var secOneZindex = parseInt(document.getElementById("secOnes9").style.zIndex);
-  if (secOneZindex === secondsOnes.length-1) {
+  if (secOneZindex === secondsOnes.length - 1) {
     secondsTens = decrementPaper("seconds-tens");
   }
   var secTenZindex = parseInt(document.getElementById("secTens5").style.zIndex);
-  if (secTenZindex === secondsTens.length-1) {
+  if (secTenZindex === secondsTens.length - 1) {
     minutesOnes = decrementPaper("minutes-ones");
   }
   var minOneZindex = parseInt(document.getElementById("minOnes9").style.zIndex);
-  if (minOneZindex === minutesOnes.length-1) {
-    decrementPaper("minutes-tens");
+  var minTenZindex = parseInt(document.getElementById("minTens0").style.zIndex);
+  if (minTenZindex !== 5) {
+    if (minOneZindex === minutesOnes.length - 1) {
+      decrementPaper("minutes-tens");
+    }
   }
 }
 
@@ -42,9 +47,12 @@ function decrementPaper(cls) {
   }
   return arrCls;
 }
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#btnStart").click(function () {
+    setInterval(myTimer, 1000);
+  });
 
 
-  
+
 
 });
