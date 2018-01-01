8d5e00c518465e59afd1080803db2236c6895780
mrcreel <michaelrcreel@gmail.com>
2018-01-01 16:01:11 -0600

    Submitted to FCC

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,83 +1,70 @@
+var endTime;
+var startTime;
+var countdown;
+var defaultCountdown = 1500000;
+var x;
+var myClock;
+
+
 function updateClock(time) {
-  console.log(time);
-  var countdownMinutesTens = Math.floor(time / 600000);
-  time = time - (countdownMinutesTens * 600000);
-  /* */
-  var countdownMinutesOnes = Math.floor((time / 60000));
-  time = time - (countdownMinutesOnes * 60000);
-  /* */
-  var countdownSecondsTens = Math.floor(time / 10000);
-  time = time - (countdownSecondsTens * 10000);
-  /* */
-  var countdownSecondsOnes = Math.floor(time / 1000);
-  time = time - (countdownSecondsOnes * 1000);
-  /**/
-  document.getElementById('minutesTens').innerHTML = countdownMinutesTens;
-  document.getElementById('minutesOnes').innerHTML = countdownMinutesOnes;
-  document.getElementById('secondsTens').innerHTML = countdownSecondsTens;
-  document.getElementById('secondsOnes').innerHTML = countdownSecondsOnes;
-}
+  if (countdown < 1000 || countdown > 5999000) {
+    countdown = defaultCountdown;
+  }
 
+  var countdownMinutes = Math.floor(countdown / 60000);
+  time = time - countdownMinutes * 60000;
+  //
+  var countdownSeconds = Math.floor(time / 1000);
+  time = time - countdownSeconds * 1000;
+  //
+  countdownMinutes = ('0' + countdownMinutes).slice(-2);
+  countdownSeconds = ('0' + countdownSeconds).slice(-2);
+
+  document.getElementById('minutes').innerHTML = countdownMinutes;
+  document.getElementById('seconds').innerHTML = countdownSeconds;
+}
 function myTimer() {
-  // var minTensZindex = parseInt(document.getElementById("minTens0").style.zIndex);
-
-  secondsOnes = decrementPaper('seconds-ones');
-
-  var secOneZindex = parseInt(document.getElementById('secOnes9').style.zIndex);
-  if (secOneZindex === secondsOnes.length - 1) {
-    secondsTens = decrementPaper('seconds-tens');
+  x = document.getElementById('myAudio');
+  var timeRemaining = new Date(endTime - Date.parse(new Date()));
+  var min = ('0' + timeRemaining.getMinutes()).slice(-2);
+  var sec = ('0' + timeRemaining.getSeconds()).slice(-2);
+  if (Date.parse(timeRemaining) <= 0) {
+    clearInterval(myClock);
+    x.play();
   }
-  /* */
-  var minTenZindex = parseInt(document.getElementById('minTens0').style.zIndex);
-  var minOneZindex = parseInt(document.getElementById('minOnes9').style.zIndex);
-  var secTenZindex = parseInt(document.getElementById('secTens5').style.zIndex);
-  /* */
-  if (minOneZindex !== 8) {
-    if (secTenZindex === secondsTens.length - 1) {
-      minutesOnes = decrementPaper('minutes-ones');
-    }
-  }
-  /* */
-  if (minTenZindex !== 5) {
-    if (minOneZindex === minutesOnes.length - 1) {
-      decrementPaper('minutes-tens');
-    }
-  }
+  console.log(min + ':', sec + '(' + Date.parse(timeRemaining) + ')');
+  document.getElementById('minutes').innerHTML = min;
+  document.getElementById('seconds').innerHTML = sec;
 }
-$(document).ready(function () {
-  var defaultCountdown = 2115000;
-  var countdown = defaultCountdown;
-
+$(document).ready(function() {
+  countdown = defaultCountdown;
   updateClock(countdown);
-  $('#btnStart').click(function () {
-    var currentTimeId = Date.parse(new Date());
-    setInterval(myTimer, 500);
+
+  $('#btnStart').click(function() {
+    startTime = Date.parse(new Date());
+    endTime = new Date(startTime + countdown);
+    myClock = setInterval(myTimer, 500);
   });
 
-  $('#btnMinutesUp').click(function () {
+  $('#btnMinutesUp').click(function() {
     countdown += 60000;
     updateClock(countdown);
   });
-  $('#btnMinutesDown').click(function () {
+  $('#btnMinutesDown').click(function() {
     countdown -= 60000;
-    if (countdown > 0) {
-      updateClock(countdown);
-    } else {
-      countdown = defaultCountdown;
-      updateClock(countdown);
-    }
+    updateClock(countdown);
   });
-  $('#btnSecondsUp').click(function () {
+  $('#btnSecondsUp').click(function() {
     countdown += 1000;
     updateClock(countdown);
   });
-  $('#btnSecondsDown').click(function () {
+  $('#btnSecondsDown').click(function() {
     countdown -= 1000;
-    if (countdown > 0) {
-      updateClock(countdown);
-    } else {
-      countdown = defaultCountdown;
-      updateClock(countdown);
-    }
+    updateClock(countdown);
+  });
+  $('#btnStop').click(function() {
+    x.pause();
+    clearInterval(myClock);
+    updateClock(countdown);
   });
 });
