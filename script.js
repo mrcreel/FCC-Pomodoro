50f7af99ea9499961c2d52d39a881cacdd81edc9
mrcreel <michaelrcreel@gmail.com>
2017-12-31 20:51:03 -0600

    Next try

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
+/* 
 var ele = "";
 var elezIndex = 0;
-/*  var myVar =  setInterval(myTimer, 1000); */
+var myVar =  setInterval(myTimer, 1000); 
+*/
+
+function updateClock(time) {
+  console.log(time);
+  var countdownMinutesTens = Math.floor(time / 600000);
+  time = time - (countdownMinutesTens * 600000);
+  /* */
+  var countdownMinutesOnes = Math.floor((time / 60000));
+  time = time - (countdownMinutesOnes * 60000);
+  /* */
+  var countdownSecondsTens = Math.floor(time / 10000);
+  time = time - (countdownSecondsTens * 10000);
+  /* */
+  var countdownSecondsOnes = Math.floor(time / 1000);
+  time = time - (countdownSecondsOnes * 1000);
+  /**/
+  document.getElementById("minutesTens").innerHTML = countdownMinutesTens;
+  document.getElementById("minutesOnes").innerHTML = countdownMinutesOnes;
+  document.getElementById("secondsTens").innerHTML = countdownSecondsTens;
+  document.getElementById("secondsOnes").innerHTML = countdownSecondsOnes;
+}
 
 function myTimer() {
   // var minTensZindex = parseInt(document.getElementById("minTens0").style.zIndex);
 
-  var secondsOnes;
-  var secondsTens = [];
-  var minutesOnes = [];
-  // var minutesTens = [];
-
   secondsOnes = decrementPaper("seconds-ones");
-
-  console.log(parseInt(document.getElementById("minTens0").style.zIndex));
 
   var secOneZindex = parseInt(document.getElementById("secOnes9").style.zIndex);
   if (secOneZindex === secondsOnes.length - 1) {
     secondsTens = decrementPaper("seconds-tens");
   }
-  var secTenZindex = parseInt(document.getElementById("secTens5").style.zIndex);
-  if (secTenZindex === secondsTens.length - 1) {
-    minutesOnes = decrementPaper("minutes-ones");
-  }
-  var minOneZindex = parseInt(document.getElementById("minOnes9").style.zIndex);
+  /* */
   var minTenZindex = parseInt(document.getElementById("minTens0").style.zIndex);
+  var minOneZindex = parseInt(document.getElementById("minOnes9").style.zIndex);
+  var secTenZindex = parseInt(document.getElementById("secTens5").style.zIndex);
+  /* */
+  if (minOneZindex !== 8) {
+    if (secTenZindex === secondsTens.length - 1) {
+      minutesOnes = decrementPaper("minutes-ones");
+    }
+  }
+  /* */
   if (minTenZindex !== 5) {
     if (minOneZindex === minutesOnes.length - 1) {
       decrementPaper("minutes-tens");
@@ -48,8 +68,40 @@ function decrementPaper(cls) {
   return arrCls;
 }
 $(document).ready(function () {
+  var defaultCountdown = 2115000;
+  var countdown = defaultCountdown;
+
+  updateClock(countdown);
   $("#btnStart").click(function () {
-    setInterval(myTimer, 1000);
+    var currentTimeId = Date.parse(new Date());
+    setInterval(myTimer, 500);
+  });
+
+  $("#btnMinutesUp").click(function () {
+    countdown += 60000;
+    updateClock(countdown);
+  });
+  $("#btnMinutesDown").click(function () {
+    countdown -= 60000;
+    if (countdown > 0) {
+      updateClock(countdown);
+    } else {
+      countdown = defaultCountdown;
+      updateClock(countdown);
+    }
+  });
+  $("#btnSecondsUp").click(function () {
+    countdown += 1000;
+    updateClock(countdown);
+  });
+  $("#btnSecondsDown").click(function () {
+    countdown -= 1000;
+    if (countdown > 0) {
+      updateClock(countdown);
+    } else {
+      countdown = defaultCountdown;
+      updateClock(countdown);
+    }
   });
 
 
